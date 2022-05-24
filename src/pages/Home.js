34dc0cefd4d819fd0b2ab440bdb17df1da4f96df
Ajiharan <baskaranajiharan1243@gmail.com>
2022-05-24 15:30:11 +0530

    TASK-004: background image & collapse button added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ImageContainer from "../components/ImageContainer";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <ImageContainer />
     </div>
   );
 };
